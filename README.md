dc83f2133fb117771559c20b2c4583c8eca242b3
Kayra Czerniewicz T <31418061+Kayracz@users.noreply.github.com>
2023-11-27 22:47:15 -0400

    created vite project and started adding frontend functionality and backend conexion to my todo app

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # TODO app (frontend)
-
 Repositorio para la parte frontend de la app de ToDo
---
+++ src/App.jsx
@@ -0,0 +1,53 @@
+import React, { useEffect, useState } from 'react';
+import axios from "axios";
+
+import './App.css'
+
+function App() {
+  const [taskList, setTaskList] = useState([]);
+  const [editTask, setEditTask] = useState({});
+
+  useEffect(() => {
+    getTasks();
+  }, []);
+
+  const getTasks = async () => {
+    try {
+      const {data} = await axios.get("http://localhost:3001/tasks");
+      setTaskList(data);
+    } catch (error) {
+      console.error("WRONG")
+    }
+  }
+
+  const postTask = async () => {
+    try {
+     await axios.post("http://localhost:3001/tasks", editTask) 
+    } catch (error) {
+      console.error("No se puede subir la tarea ")
+    }
+  }
+
+  const handleOnChange = (event) => {
+    setEditTask({
+      title: event.target.value,
+      status: "TO DO"
+    })
+    console.log("editTask", editTask);
+  }
+
+  return (
+    <>
+      <form onSubmit={postTask}>
+        <input type='text' onChange={handleOnChange}></input>
+      </form>
+
+      {taskList.map((task, index) => {
+        return <p key={index}>{task.title}</p>;
+      })}
+    </>
+  );
+}
+
+
+export default App

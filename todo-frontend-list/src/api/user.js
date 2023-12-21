abdc7d12249e930ca093d4f7b631f7271654f9e8
Carmen Plaza <carmenplazaromero@gmail.com>
2023-12-21 20:28:04 +0100

    aplicacion terminada

--- todo-frontend-list/src/api/user.js
+++
@@ -1,3 +0,0 @@
-import axios from './axios';
-
-export const getAllUsers = ()=> axios.get('/user');
--- todo-frontend-list/src/components/Navbar.jsx
+++
@@ -1,6 +0,0 @@
-import { Link } from 'react-router-dom';
-const Navbar = (user, isAuthenticated) => {
-  return <div>Navbar</div>;
-};
-
-export default Navbar;

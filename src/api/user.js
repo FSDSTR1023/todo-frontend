62182cec9ba169bc144d496d7b0d12cd9a53f767
Juan Mencía <juamm99@gmail.com>
2023-12-22 02:27:44 +0100

    Revert "Creación de la aplicación"
    This reverts commit 02135d29ec489515312309167c6d3ed65f991612.

--- src/api/user.js
+++
@@ -1,5 +0,0 @@
-import axios from './axios.js';
-
-export const getUsers = () => axios.get('/users');
-export const logIn = (user) => axios.get('/users/login', user);
-export const registerRequest = (user) => axios.post('users/login', user);
